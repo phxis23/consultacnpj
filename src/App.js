2004ae5c4cfc06d442f00599f39496a8b5a03cf7
Pedro Higor <phlindinho9@gmail.com>
2024-08-16 16:24:53 -0300

    primeira versão

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './style.css'; 
+import api from "./services/api" 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [input, setInput] = useState(''); 
+  const [cnpj, setCNPJ] = useState({}); 
+
+ 
+  async function handleSearch(){ 
+
+    if(input === ''){ 
+      alert("Preencha algum CNPJ!") 
+      return; 
+    } 
+
+  try{  
+  const response = await api.get(`${input}`) 
+    setCNPJ(response.data) 
+    setInput("") 
+  }catch{ 
+    alert("Erro ao buscar CNPJ!")
+    setInput("") 
+ }
+
+}
+  return ( 
+  <div className="container">
+  <h1 className="title">Buscador CNPJ</h1> 
+  
+  <div className="containerInput"> 
+    <input 
+       type="text"
+       placeholder="Digite seu CNPJ..."
+       value={input}
+       onChange={(e) => setInput(e.target.value)}
+   />
+  
+  <button className="buttonSearch" onClick={handleSearch}> 
+      <FiSearch size={25} color="#FFF"/>
+    </button> 
+
+ 
+  </div>
+   {Object.keys(cnpj).length > 0 && ( 
+    <main className="main"> 
+      <h2>Razão Social: {cnpj.razao_social}</h2>
+      <span>Fundação: {cnpj.data_inicio_atividade}</span> 
+      <span>Situação Cadastral: {cnpj.descricao_situacao_cadastral}</span>
+      <span>Contato: {cnpj.ddd_fax}</span>
+    </main> 
+  )} 
+   
+  </div>
+  );
+} 
+  
+export default App; 
+  
